ee2c00ff8e944200ac78c0a87a9400f0def7ff66
Navneet Sharma <navneet.sharma@nagarro.com>
2024-01-10 17:33:50 +0530

    implement react toolkit version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
+import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
-import { useTheme } from "../context/useTheme"
+import { ThemeState, toggleTheme } from "../store/themeSlice";
 
 const Navbar = () => {
 
-    const { theme, toggleTheme } = useTheme();
+    const isDarkMode = useSelector<ThemeState>(state => state.isDarkMode);
+    const theme = isDarkMode ? "dark" : "light"
+    const dispatch = useDispatch();
 
     return (
         <nav className={`navbar ${theme}`}>
@@ -16,7 +19,7 @@ const Navbar = () => {
                 <label>
                     <input
                         type="checkbox"
-                        onChange={() => toggleTheme()}
+                        onChange={() => dispatch(toggleTheme())}
                         checked={theme === "dark"}
                     />
                     <span className="slider round"></span>
